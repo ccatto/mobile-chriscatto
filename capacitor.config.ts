18ec231370a592ce19f4a5861e6511822d4d1bea
Catto <catto@live.com>
2024-05-14 10:18:50 -0400

    added capacitor for both ios

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'mobile-chriscatto',
+  webDir: 'out'
+  // , "server": {
+  //   "androidScheme": "https"
+  // }
+};
+
+export default config;
