6e0acfd623b9b65ddf2a42d12b0f601b4cbe1ec2
Catto <catto@live.com>
2024-05-15 15:33:49 -0400

    added iconic react packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,23 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+/* Ionic CSS Imports */
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+// /* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+// /* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,7 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="hydrated">
       <body className={inter.className}>{children}</body>
     </html>
   );
